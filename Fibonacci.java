cf2d7b77b2f8ba36c8084caaa5be14f353920d9f
Aakanksha Patil <aakankshapatil322@gmail.com>
2026-09-01 22:30:44 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many Fibonacci terms to print? ");
+        int terms = scanner.nextInt();
+        
+        int first = 0, second = 1;
+        System.out.print("Series: ");
+        
+        for (int i = 1; i <= terms; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        scanner.close();
+    }
+}
+
